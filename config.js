7700b4a989de9acfd227df9d6fa473b024be87d8
zenoxx-xeon <110280421+zenoxx-xeon@users.noreply.github.com>
2023-04-30 19:33:20 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,10 +7,10 @@ global.autoReadAll = false
 global.antitags = true
 
 
-global.Owner = ['918101187835']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
-global.OwnerNumber = ['918101187835']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
+global.Owner = ['916235541282']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
+global.OwnerNumber = ['916235541282']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
 global.ownertag = ['918101187835']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
-global.BotName = "Miku"
+global.BotName = "Zenox"
 global.packname = "Miku bot"
 global.author = "By: Fantox"
 global.OwnerName = "Fantox"
@@ -20,7 +20,7 @@ global.sessionName = "session"  // -- Don't change otherwise bot will not work -
 
 
 
-global.prefa = ['-'] // Prefix ---  change it to anythong you want except '@' //
+global.prefa = ['.'] // Prefix ---  change it to anythong you want except '@' //
 
 
 global.location = "West Bengal, India"
